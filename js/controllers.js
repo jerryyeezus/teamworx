a8459980fc4008b11bcfefce6cb52e449489935f
Thang Nguyen <thang911pk@gmail.com>
2015-02-15 04:10:38 -0500

    Fix NavBar and LO

--- js/controllers.js
+++ js/controllers.js
@@ -59,3 +59,32 @@ mainControllers.controller('CredentialsController', ['$location', '$scope', 'Aut
 
 }]);
 
+
+mainControllers.controller('NavigationController', ['$location', '$scope', 'Authentication', '$rootScope', function($location, $scope, Authentication, $rootScope) {
+    activate();
+
+    function activate() {
+        if (Authentication.isAuthenticated()) {
+            $location.url('/portal');
+        }
+    }
+
+    $rootScope.isActive = function(viewLocation) {
+        return viewLocation === $location.path();
+    }
+
+    $scope.register = function (formData) {
+        Authentication.register(formData);
+    }
+
+    $scope.login = function(formData) {
+        Authentication.login(formData)
+    }
+
+    $scope.logout = function() {
+        Authentication.logout();
+    }
+
+}]);
+
+
