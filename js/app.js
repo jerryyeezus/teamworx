a14275d35d87bf4e1d3f3c3fe529c859725b5d5d
Jerry Yee <yee@gatech.edu>
2015-02-27 13:43:27 -0500

    silly me

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var myApp = angular.module('myApp', [
 ]);
 
 // flag
-var DEBUG = true;
+var DEBUG = false;
 
 var server_url = 'http://ec2-54-69-18-202.us-west-2.compute.amazonaws.com:8000/';
 if (DEBUG)
