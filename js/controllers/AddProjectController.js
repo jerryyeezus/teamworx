328b9db1f088861098dcc6ff35fb9b8cb3250dc3
Thang Nguyen <thang911pk@gmail.com>
2015-04-08 02:53:10 -0400

    Add project_preference, fix drag/drop, fix group_profile in main

--- js/controllers/AddProjectController.js
+++ js/controllers/AddProjectController.js
@@ -4,21 +4,14 @@ mainControllers.controller('AddProjectController',
         function ($http, $location, Authentication, $scope, $rootScope, $cookieStore,
                   $modal, $window, toaster, $modalInstance, $stateParams, ass_service, add_project_service) {
             $scope.the_user = Authentication.getAuthenticatedAccount()['email'];
-            $http.get(Authentication.server_url + 'add_project' + ass_service.getAssignment.pk).success(function(response) {
-                $scope.projects = response;
-                $scope.myForm = {
-                    'project_number': $scope.projects.length + 1
-                };
-            });
 
             $scope.submit = function () {
                 var dataObject = {};
-                dataObject['ass_fk'] = ass_service.getAssignment.pk;
-                dataObject['project_number'] = $scope.myForm.project_number;
-                dataObject['project_title'] = $scope.myForm.project_title;
-                dataObject['project_text'] = $scope.myForm.project_text;
+                dataObject['ass_fk'] = $cookieStore.get('assignment_pk');
+                dataObject['description'] = $scope.myForm.project_description;
+                dataObject['name'] = $scope.myForm.project_name;
 
-
+                console.log(dataObject);
                 var responsePromise = $http.post(Authentication.server_url + 'add_project/', dataObject, {});
                 responsePromise.success(function () {
                     add_project_service.setDirty();
